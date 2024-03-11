110e6bda60dd50e484098235f3e1e94d9017914f
bangkit16 <bangkitcaniago@gmail.com>
2024-03-11 15:17:35 +0700

    Minggu 4 - Praktikum 2.6

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,17 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/penjualan', [HomeController::class, 'penjualan']);
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
 
 Route::prefix('category')->group(function () {
     Route::get('/baby', [ProductController::class, 'baby']);
