45dbb4422e68b18e3abe3a50a4898a7a0d327408
bangkit16 <bangkitcaniago@gmail.com>
2024-03-11 12:14:40 +0700

    Minggu 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,14 +16,27 @@ class UserController extends Controller
 
     public function index()
     {
-        $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 3,
-        ];
-        UserModel::insert($data);
-        $user = UserModel::all();
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 2);
+        // $user = UserModel::findOr(1,function () {
+
+        // });
+        // $user = UserModel::where('level_id','>',3)->firstOr(function () {
+
+        // });
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
